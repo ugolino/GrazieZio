781ff5bac62df238876fefae70a7b5c0da84e957
ugolino <dan.ugolini@gmail.com>
2015-02-10 18:39:53 +0100

    added validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,8 @@
 class Task < ActiveRecord::Base
-	belongs_to :user
+	belongs_to :user 
+	validates :user_id, presence: true
+	validates :content, presence: true
+	
 	auto_html_for :content do
 	html_escape
 	image
